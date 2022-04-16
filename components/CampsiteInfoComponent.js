22feda6da857406395af4b2b217c6435f9118057
Reema Sabatin <rmsabatin@up.edu.ph>
2022-04-16 11:13:19 +0800

    Week 1 Assignment

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, FlatList } from "react-native";
 import { Card } from "react-native-elements";
 import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
+
+function RenderComments({ comments }) {
+  const renderCommentItem = ({ item }) => {
+    return (
+      <View style={{ margin: 10 }}>
+        <Text style={{ fontSize: 14 }}>{item.text}</Text>
+        <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+        <Text
+          style={{ fontSize: 12 }}
+        >{`-- ${item.author}, ${item.date}`}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <Card title="Comments">
+      <FlatList
+        data={comments}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </Card>
+  );
+}
 
 function RenderCampsite({ campsite }) {
   if (campsite) {
@@ -22,6 +47,7 @@ class CampsiteInfo extends Component {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      comments: COMMENTS,
     };
   }
 
