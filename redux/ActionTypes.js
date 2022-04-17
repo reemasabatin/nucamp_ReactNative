3c770627ad083045a5af325c8493484675fd9d7e
Reema Sabatin <rmsabatin@up.edu.ph>
2022-04-17 17:02:05 +0800

    Swipe and Delete Favorites

--- redux/ActionTypes.js
+++ redux/ActionTypes.js
@@ -15,5 +15,7 @@ export const PARTNERS_FAILED = "PARTNERS_FAILED";
 
 export const ADD_FAVORITE = "ADD_FAVORITE";
 
-//Task 2 o 3 ata ni tung iexport nimo
+// Week 2 workshop Task 2 o 3 ata ni tung iexport nimo
 export const ADD_COMMENT = "ADD_COMMENT";
+
+export const DELETE_FAVORITE = "DELETE_FAVORITE";
